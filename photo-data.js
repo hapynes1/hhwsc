c2af5e9499bf2daf152aaf14f4977674a37ef558
hapynes1 <2453419800@qq.com>
2026-06-27 23:20:27 +0800

    Add 13 homepage photos

--- photo-data.js
+++ photo-data.js
@@ -1,471 +1,536 @@
 window.PHOTO_LIST = [
   {
     "src": "assets/photos/photo-001.webp",
+    "original": "图片/00059172865b8c9bdd2e5b5538143920.png",
+    "name": "00059172865b8c9bdd2e5b5538143920"
+  },
+  {
+    "src": "assets/photos/photo-002.webp",
+    "original": "图片/040c8780625fde10e24e2ad884414324.png",
+    "name": "040c8780625fde10e24e2ad884414324"
+  },
+  {
+    "src": "assets/photos/photo-003.webp",
     "original": "图片/107216.JPG",
     "name": "107216"
   },
   {
-    "src": "assets/photos/photo-002.webp",
+    "src": "assets/photos/photo-004.webp",
     "original": "图片/111951.JPG",
     "name": "111951"
   },
   {
-    "src": "assets/photos/photo-003.webp",
+    "src": "assets/photos/photo-005.webp",
     "original": "图片/125057.JPG",
     "name": "125057"
   },
   {
-    "src": "assets/photos/photo-004.webp",
+    "src": "assets/photos/photo-006.webp",
     "original": "图片/127384.JPG",
     "name": "127384"
   },
   {
-    "src": "assets/photos/photo-005.webp",
+    "src": "assets/photos/photo-007.webp",
     "original": "图片/127444.JPG",
     "name": "127444"
   },
   {
-    "src": "assets/photos/photo-006.webp",
+    "src": "assets/photos/photo-008.webp",
     "original": "图片/135961.JPG",
     "name": "135961"
   },
   {
-    "src": "assets/photos/photo-007.webp",
+    "src": "assets/photos/photo-009.webp",
     "original": "图片/136854.JPG",
     "name": "136854"
   },
   {
-    "src": "assets/photos/photo-008.webp",
+    "src": "assets/photos/photo-010.webp",
     "original": "图片/146522.JPG",
     "name": "146522"
   },
   {
-    "src": "assets/photos/photo-009.webp",
+    "src": "assets/photos/photo-011.webp",
     "original": "图片/158041.JPG",
     "name": "158041"
   },
   {
-    "src": "assets/photos/photo-010.webp",
+    "src": "assets/photos/photo-012.webp",
     "original": "图片/158682.JPG",
     "name": "158682"
   },
   {
-    "src": "assets/photos/photo-011.webp",
+    "src": "assets/photos/photo-013.webp",
     "original": "图片/158791.JPG",
     "name": "158791"
   },
   {
-    "src": "assets/photos/photo-012.webp",
+    "src": "assets/photos/photo-014.webp",
     "original": "图片/171173.JPG",
     "name": "171173"
   },
   {
-    "src": "assets/photos/photo-013.webp",
+    "src": "assets/photos/photo-015.webp",
     "original": "图片/171174.JPG",
     "name": "171174"
   },
   {
-    "src": "assets/photos/photo-014.webp",
+    "src": "assets/photos/photo-016.webp",
     "original": "图片/172173.JPG",
     "name": "172173"
   },
   {
-    "src": "assets/photos/photo-015.webp",
+    "src": "assets/photos/photo-017.webp",
     "original": "图片/178036.JPG",
     "name": "178036"
   },
   {
-    "src": "assets/photos/photo-016.webp",
+    "src": "assets/photos/photo-018.webp",
     "original": "图片/192181.JPG",
     "name": "192181"
   },
   {
-    "src": "assets/photos/photo-017.webp",
+    "src": "assets/photos/photo-019.webp",
     "original": "图片/234663106A5F8C9E364D5611C84A7A50.png",
     "name": "234663106A5F8C9E364D5611C84A7A50"
   },
   {
-    "src": "assets/photos/photo-018.webp",
+    "src": "assets/photos/photo-020.webp",
+    "original": "图片/2d9e51d9faca35a8846004854b19206d.png",
+    "name": "2d9e51d9faca35a8846004854b19206d"
+  },
+  {
+    "src": "assets/photos/photo-021.webp",
+    "original": "图片/3e2f9f854ea56d2b8ab4a93d09b11e79.png",
+    "name": "3e2f9f854ea56d2b8ab4a93d09b11e79"
+  },
+  {
+    "src": "assets/photos/photo-022.webp",
     "original": "图片/3fb4e5e23ffbf642b649d0a368f7f578.png",
     "name": "3fb4e5e23ffbf642b649d0a368f7f578"
   },
   {
-    "src": "assets/photos/photo-019.webp",
+    "src": "assets/photos/photo-023.webp",
     "original": "图片/51c3201eea34e8fd3a12f5ab4444c538.png",
     "name": "51c3201eea34e8fd3a12f5ab4444c538"
   },
   {
-    "src": "assets/photos/photo-020.webp",
+    "src": "assets/photos/photo-024.webp",
+    "original": "图片/673fbeb870f836f02a72fd69d68df8fb.png",
+    "name": "673fbeb870f836f02a72fd69d68df8fb"
+  },
+  {
+    "src": "assets/photos/photo-025.webp",
+    "original": "图片/70e68fd0da577cd30d8ba58ad3fd8fc5.png",
+    "name": "70e68fd0da577cd30d8ba58ad3fd8fc5"
+  },
+  {
+    "src": "assets/photos/photo-026.webp",
     "original": "图片/7b61d61c984694926e870f43c2c025ed.png",
     "name": "7b61d61c984694926e870f43c2c025ed"
   },
   {
-    "src": "assets/photos/photo-021.webp",
+    "src": "assets/photos/photo-027.webp",
     "original": "图片/9514f14bcd5b65df14650284158bcab1.png",
     "name": "9514f14bcd5b65df14650284158bcab1"
   },
   {
-    "src": "assets/photos/photo-022.webp",
+    "src": "assets/photos/photo-028.webp",
     "original": "图片/9a013ce505b2dda00ff8426ff686bfe0.png",
     "name": "9a013ce505b2dda00ff8426ff686bfe0"
   },
   {
-    "src": "assets/photos/photo-023.webp",
+    "src": "assets/photos/photo-029.webp",
+    "original": "图片/a13d50af4c9e50230fe74f471f58f28c.png",
+    "name": "a13d50af4c9e50230fe74f471f58f28c"
+  },
+  {
+    "src": "assets/photos/photo-030.webp",
     "original": "图片/a709c7954be231fb812cef59bd1aece8.JPG",
     "name": "a709c7954be231fb812cef59bd1aece8"
   },
   {
-    "src": "assets/photos/photo-024.webp",
+    "src": "assets/photos/photo-031.webp",
+    "original": "图片/a75d34bcae21293476ee00de73b9568b.png",
+    "name": "a75d34bcae21293476ee00de73b9568b"
+  },
+  {
+    "src": "assets/photos/photo-032.webp",
     "original": "图片/b11aab9f760bc3c49b80b506a92f284f.JPG",
     "name": "b11aab9f760bc3c49b80b506a92f284f"
   },
   {
-    "src": "assets/photos/photo-025.webp",
+    "src": "assets/photos/photo-033.webp",
+    "original": "图片/c2ef752e436bf53cceaaa41cb451f16a.png",
+    "name": "c2ef752e436bf53cceaaa41cb451f16a"
+  },
+  {
+    "src": "assets/photos/photo-034.webp",
     "original": "图片/dji_mimo_20260503_184624_20260503184624_178185730.JPG",
     "name": "dji_mimo_20260503_184624_20260503184624_178185730"
   },
   {
-    "src": "assets/photos/photo-026.webp",
+    "src": "assets/photos/photo-035.webp",
+    "original": "图片/efae6ffb3dd3db6c812f5c518e09ec18.png",
+    "name": "efae6ffb3dd3db6c812f5c518e09ec18"
+  },
+  {
+    "src": "assets/photos/photo-036.webp",
     "original": "图片/f1a8ab1c567dbbd01147d1ff71582fd6.JPG",
     "name": "f1a8ab1c567dbbd01147d1ff71582fd6"
   },
   {
-    "src": "assets/photos/photo-027.webp",
+    "src": "assets/photos/photo-037.webp",
     "original": "图片/f421a4bcbe56454cf0b8cd0dabac98eb.png",
     "name": "f421a4bcbe56454cf0b8cd0dabac98eb"
   },
   {
-    "src": "assets/photos/photo-028.webp",
+    "src": "assets/photos/photo-038.webp",
+    "original": "图片/f64cba639c1d439361af927d47c83a3c.png",
+    "name": "f64cba639c1d439361af927d47c83a3c"
+  },
+  {
+    "src": "assets/photos/photo-039.webp",
+    "original": "图片/fbd9e11a11ff5f6a5f90c8f5ea268c6f.png",
+    "name": "fbd9e11a11ff5f6a5f90c8f5ea268c6f"
+  },
+  {
+    "src": "assets/photos/photo-040.webp",
     "original": "图片/fe66879b3ec149057f9c6034c6c2d001.png",
     "name": "fe66879b3ec149057f9c6034c6c2d001"
   },
   {
-    "src": "assets/photos/photo-029.webp",
+    "src": "assets/photos/photo-041.webp",
+    "original": "图片/ffd585c92c178f7e7e2f27e2d2cbb120.png",
+    "name": "ffd585c92c178f7e7e2f27e2d2cbb120"
+  },
+  {
+    "src": "assets/photos/photo-042.webp",
     "original": "图片/image_d5adc2083fe2a807859adcf3139ee9cb.JPG",
     "name": "image_d5adc2083fe2a807859adcf3139ee9cb"
   },
   {
-    "src": "assets/photos/photo-030.webp",
+    "src": "assets/photos/photo-043.webp",
     "original": "图片/IMG_3850.JPG",
     "name": "IMG_3850"
   },
   {
-    "src": "assets/photos/photo-031.webp",
+    "src": "assets/photos/photo-044.webp",
     "original": "图片/IMG_3870.JPG",
     "name": "IMG_3870"
   },
   {
-    "src": "assets/photos/photo-032.webp",
+    "src": "assets/photos/photo-045.webp",
     "original": "图片/IMG_3871.JPG",
     "name": "IMG_3871"
   },
   {
-    "src": "assets/photos/photo-033.webp",
+    "src": "assets/photos/photo-046.webp",
     "original": "图片/IMG_3881.JPG",
     "name": "IMG_3881"
   },
   {
-    "src": "assets/photos/photo-034.webp",
+    "src": "assets/photos/photo-047.webp",
     "original": "图片/IMG_3882.JPG",
     "name": "IMG_3882"
   },
   {
-    "src": "assets/photos/photo-035.webp",
+    "src": "assets/photos/photo-048.webp",
     "original": "图片/IMG_3886.JPG",
     "name": "IMG_3886"
   },
   {
-    "src": "assets/photos/photo-036.webp",
+    "src": "assets/photos/photo-049.webp",
     "original": "图片/IMG_3903.JPG",
     "name": "IMG_3903"
   },
   {
-    "src": "assets/photos/photo-037.webp",
+    "src": "assets/photos/photo-050.webp",
     "original": "图片/IMG_4321.JPG",
     "name": "IMG_4321"
   },
   {
-    "src": "assets/photos/photo-038.webp",
+    "src": "assets/photos/photo-051.webp",
     "original": "图片/IMG_4359.JPG",
     "name": "IMG_4359"
   },
   {
-    "src": "assets/photos/photo-039.webp",
+    "src": "assets/photos/photo-052.webp",
     "original": "图片/IMG_4394.JPG",
     "name": "IMG_4394"
   },
   {
-    "src": "assets/photos/photo-040.webp",
+    "src": "assets/photos/photo-053.webp",
     "original": "图片/IMG_4414.JPG",
     "name": "IMG_4414"
   },
   {
-    "src": "assets/photos/photo-041.webp",
+    "src": "assets/photos/photo-054.webp",
     "original": "图片/IMG_5249.JPG",
     "name": "IMG_5249"
   },
   {
-    "src": "assets/photos/photo-042.webp",
+    "src": "assets/photos/photo-055.webp",
     "original": "图片/IMG_6648.JPG",
     "name": "IMG_6648"
   },
   {
-    "src": "assets/photos/photo-043.webp",
+    "src": "assets/photos/photo-056.webp",
     "original": "图片/IMG_6682.JPG",
     "name": "IMG_6682"
   },
   {
-    "src": "assets/photos/photo-044.webp",
+    "src": "assets/photos/photo-057.webp",
     "original": "图片/IMG_6684.JPG",
     "name": "IMG_6684"
   },
   {
-    "src": "assets/photos/photo-045.webp",
+    "src": "assets/photos/photo-058.webp",
     "original": "图片/IMG_6685.JPG",
     "name": "IMG_6685"
   },
   {
-    "src": "assets/photos/photo-046.webp",
+    "src": "assets/photos/photo-059.webp",
     "original": "图片/IMG_6693.JPG",
     "name": "IMG_6693"
   },
   {
-    "src": "assets/photos/photo-047.webp",
+    "src": "assets/photos/photo-060.webp",
     "original": "图片/IMG_6704.JPG",
     "name": "IMG_6704"
   },
   {
-    "src": "assets/photos/photo-048.webp",
+    "src": "assets/photos/photo-061.webp",
     "original": "图片/IMG_6716.JPG",
     "name": "IMG_6716"
   },
   {
-    "src": "assets/photos/photo-049.webp",
+    "src": "assets/photos/photo-062.webp",
     "original": "图片/IMG_6732.JPG",
     "name": "IMG_6732"
   },
   {
-    "src": "assets/photos/photo-050.webp",
+    "src": "assets/photos/photo-063.webp",
     "original": "图片/IMG_6733.JPG",
     "name": "IMG_6733"
   },
   {
-    "src": "assets/photos/photo-051.webp",
+    "src": "assets/photos/photo-064.webp",
     "original": "图片/IMG_6734.JPG",
     "name": "IMG_6734"
   },
   {
-    "src": "assets/photos/photo-052.webp",
+    "src": "assets/photos/photo-065.webp",
     "original": "图片/IMG_6746.JPG",
     "name": "IMG_6746"
   },
   {
-    "src": "assets/photos/photo-053.webp",
+    "src": "assets/photos/photo-066.webp",
     "original": "图片/IMG_6747.JPG",
     "name": "IMG_6747"
   },
   {
-    "src": "assets/photos/photo-054.webp",
+    "src": "assets/photos/photo-067.webp",
     "original": "图片/IMG_6748.JPG",
     "name": "IMG_6748"
   },
   {
-    "src": "assets/photos/photo-055.webp",
+    "src": "assets/photos/photo-068.webp",
     "original": "图片/IMG_6751.JPG",
     "name": "IMG_6751"
   },
   {
-    "src": "assets/photos/photo-056.webp",
+    "src": "assets/photos/photo-069.webp",
     "original": "图片/IMG_6752.JPG",
     "name": "IMG_6752"
   },
   {
-    "src": "assets/photos/photo-057.webp",
+    "src": "assets/photos/photo-070.webp",
     "original": "图片/IMG_6801.JPG",
     "name": "IMG_6801"
   },
   {
-    "src": "assets/photos/photo-058.webp",
+    "src": "assets/photos/photo-071.webp",
     "original": "图片/IMG_6804.JPG",
     "name": "IMG_6804"
   },
   {
-    "src": "assets/photos/photo-059.webp",
+    "src": "assets/photos/photo-072.webp",
     "original": "图片/IMG_6836.JPG",
     "name": "IMG_6836"
   },
   {
-    "src": "assets/photos/photo-060.webp",
+    "src": "assets/photos/photo-073.webp",
     "original": "图片/IMG_6851.JPG",
     "name": "IMG_6851"
   },
   {
-    "src": "assets/photos/photo-061.webp",
+    "src": "assets/photos/photo-074.webp",
     "original": "图片/IMG_6853.JPG",
     "name": "IMG_6853"
   },
   {
-    "src": "assets/photos/photo-062.webp",
+    "src": "assets/photos/photo-075.webp",
     "original": "图片/IMG_6855.JPG",
     "name": "IMG_6855"
   },
   {
-    "src": "assets/photos/photo-063.webp",
+    "src": "assets/photos/photo-076.webp",
     "original": "图片/IMG_6863.JPG",
     "name": "IMG_6863"
   },
   {
-    "src": "assets/photos/photo-064.webp",
+    "src": "assets/photos/photo-077.webp",
     "original": "图片/IMG_6864.JPG",
     "name": "IMG_6864"
   },
   {
-    "src": "assets/photos/photo-065.webp",
+    "src": "assets/photos/photo-078.webp",
     "original": "图片/IMG_6865.JPG",
     "name": "IMG_6865"
   },
   {
-    "src": "assets/photos/photo-066.webp",
+    "src": "assets/photos/photo-079.webp",
     "original": "图片/IMG_6888.JPG",
     "name": "IMG_6888"
   },
   {
-    "src": "assets/photos/photo-067.webp",
+    "src": "assets/photos/photo-080.webp",
     "original": "图片/IMG_6917.JPG",
     "name": "IMG_6917"
   },
   {
-    "src": "assets/photos/photo-068.webp",
+    "src": "assets/photos/photo-081.webp",
     "original": "图片/IMG_6928.JPG",
     "name": "IMG_6928"
   },
   {
-    "src": "assets/photos/photo-069.webp",
+    "src": "assets/photos/photo-082.webp",
     "original": "图片/IMG_6929.JPG",
     "name": "IMG_6929"
   },
   {
-    "src": "assets/photos/photo-070.webp",
+    "src": "assets/photos/photo-083.webp",
     "original": "图片/IMG_6930.JPG",
     "name": "IMG_6930"
   },
   {
-    "src": "assets/photos/photo-071.webp",
+    "src": "assets/photos/photo-084.webp",
     "original": "图片/IMG_6959.JPG",
     "name": "IMG_6959"
   },
   {
-    "src": "assets/photos/photo-072.webp",
+    "src": "assets/photos/photo-085.webp",
     "original": "图片/IMG_6960.JPG",
     "name": "IMG_6960"
   },
   {
-    "src": "assets/photos/photo-073.webp",
+    "src": "assets/photos/photo-086.webp",
     "original": "图片/IMG_6966.JPG",
     "name": "IMG_6966"
   },
   {
-    "src": "assets/photos/photo-074.webp",
+    "src": "assets/photos/photo-087.webp",
     "original": "图片/IMG_6972.JPG",
     "name": "IMG_6972"
   },
   {
-    "src": "assets/photos/photo-075.webp",
+    "src": "assets/photos/photo-088.webp",
     "original": "图片/IMG_6974.JPG",
     "name": "IMG_6974"
   },
   {
-    "src": "assets/photos/photo-076.webp",
+    "src": "assets/photos/photo-089.webp",
     "original": "图片/IMG_6979.JPG",
     "name": "IMG_6979"
   },
   {
-    "src": "assets/photos/photo-077.webp",
+    "src": "assets/photos/photo-090.webp",
     "original": "图片/IMG_6980.JPG",
     "name": "IMG_6980"
   },
   {
-    "src": "assets/photos/photo-078.webp",
+    "src": "assets/photos/photo-091.webp",
     "original": "图片/IMG_6992.JPG",
     "name": "IMG_6992"
   },
   {
-    "src": "assets/photos/photo-079.webp",
+    "src": "assets/photos/photo-092.webp",
     "original": "图片/IMG_6996.JPG",
     "name": "IMG_6996"
   },
   {
-    "src": "assets/photos/photo-080.webp",
+    "src": "assets/photos/photo-093.webp",
     "original": "图片/IMG_7068.JPG",
     "name": "IMG_7068"
   },
   {
-    "src": "assets/photos/photo-081.webp",
+    "src": "assets/photos/photo-094.webp",
     "original": "图片/IMG_7172.JPG",
     "name": "IMG_7172"
   },
   {
-    "src": "assets/photos/photo-082.webp",
+    "src": "assets/photos/photo-095.webp",
     "original": "图片/IMG_7230.JPG",
     "name": "IMG_7230"
   },
   {
-    "src": "assets/photos/photo-083.webp",
+    "src": "assets/photos/photo-096.webp",
     "original": "图片/IMG_7325.JPG",
     "name": "IMG_7325"
   },
   {
-    "src": "assets/photos/photo-084.webp",
+    "src": "assets/photos/photo-097.webp",
     "original": "图片/IMG_7382.JPG",
     "name": "IMG_7382"
   },
   {
-    "src": "assets/photos/photo-085.webp",
+    "src": "assets/photos/photo-098.webp",
     "original": "图片/IMG_7446.PNG",
     "name": "IMG_7446"
   },
   {
-    "src": "assets/photos/photo-086.webp",
+    "src": "assets/photos/photo-099.webp",
     "original": "图片/IMG_7828.JPG",
     "name": "IMG_7828"
   },
   {
-    "src": "assets/photos/photo-087.webp",
+    "src": "assets/photos/photo-100.webp",
     "original": "图片/IMG_7832.JPG",
     "name": "IMG_7832"
   },
   {
-    "src": "assets/photos/photo-088.webp",
+    "src": "assets/photos/photo-101.webp",
     "original": "图片/IMG_7881.JPG",
     "name": "IMG_7881"
   },
   {
-    "src": "assets/photos/photo-089.webp",
+    "src": "assets/photos/photo-102.webp",
     "original": "图片/IMG_7882.JPG",
     "name": "IMG_7882"
   },
   {
-    "src": "assets/photos/photo-090.webp",
+    "src": "assets/photos/photo-103.webp",
     "original": "图片/IMG_7963.JPG",
     "name": "IMG_7963"
   },
   {
-    "src": "assets/photos/photo-091.webp",
+    "src": "assets/photos/photo-104.webp",
     "original": "图片/IMG_7970.JPG",
     "name": "IMG_7970"
   },
   {
-    "src": "assets/photos/photo-092.webp",
+    "src": "assets/photos/photo-105.webp",
     "original": "图片/IMG_7971.JPG",
     "name": "IMG_7971"
   },
   {
-    "src": "assets/photos/photo-093.webp",
+    "src": "assets/photos/photo-106.webp",
     "original": "图片/IMG_8094.JPG",
     "name": "IMG_8094"
   },
   {
-    "src": "assets/photos/photo-094.webp",
+    "src": "assets/photos/photo-107.webp",
     "original": "图片/IMG_8272.JPG",
     "name": "IMG_8272"
   }
